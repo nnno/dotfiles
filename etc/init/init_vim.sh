3f8d9b1976fec991626060b828a41a2f8ad9c0d4
nnno <nnno@nnno.jp>
2016-04-11 04:06:05 +0900

    インストールスクリプトを修正。一旦何もしないことにした

--- etc/init/init_vim.sh
+++ etc/init/init_vim.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 
-# First, clear directory.
-rm -rf ~/.vim/bundle/*
-
-# Step 1: Install NeoBundle.
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
-# Step 2: Install configured bundles.
-vim +NeoBundleInstall +qall
-
