45de331a19f93d78f8803e0a42c49b885784bd11
nnno <nnno@nnno.jp>
2023-02-19 04:33:25 +0900

    マルチユーザ環境を念頭にhomebrewをユーザディレクトリ配下にインストールするように変更

--- etc/init/osx/install_homebrew.sh
+++ etc/init/osx/install_homebrew.sh
@@ -7,5 +7,10 @@ if type brew > /dev/null 2>&1; then
     exit
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+mkdir $HOME/.homebrew
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+
+echo 'Add the following to your .zshrc'
+echo 'export PATH=$HOME/.homebrew/bin:$PATH'
+echo 'export HOMEBREW_CACHE=$HOME/.homebrew/caches'
 
